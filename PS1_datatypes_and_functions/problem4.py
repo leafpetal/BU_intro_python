6458300a260f1038360095357266495816f4ede1
Soomin Oh <87401748+leafpetal@users.noreply.github.com>
2022-06-09 04:57:07 -0400

    Create problem4.py

---
+++ PS1_datatypes_and_functions/problem4.py
@@ -0,0 +1,43 @@
+# problem4.py - Problem Set 1, Problem 4
+#
+# Functions on strings and lists, part 2
+
+
+
+# function 1
+def reverse(s):
+    """reverse the sequences of characters in input string"""
+    return s[::-1]
+
+
+# function 2
+def ends_match(s):
+    """first letter and last letter of input should match"""
+    if s[0] == s[-1]:
+        return True
+    else:
+        return False
+    
+    
+# function 3
+def replace_start(values, new_start_vals):
+    """replace first n elements of list values with new_start_vals elements"""
+    a = len(values)
+    b = len(new_start_vals)
+    if b < a:
+        return new_start_vals[:] + values[b:]
+    else:
+        return new_start_vals
+
+
+# function 4
+def adjust(s, length):
+    """pad s with its last character if it is too short"""
+    a = len(s)
+    if a < length:
+        b = length - a
+        return s + s[-1]*b
+    elif a == length:
+        return s
+    else:
+        return s[:length]
